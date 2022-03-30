757d8423b9c786a6e4c36804ccf5afce29a50a29
KCH <doolysmile1@naver.com>
2022-03-31 00:11:17 +0900

    숫자 문자열과 영단어

---
+++ programmers/lv1/WordToNumReplace.java
@@ -0,0 +1,16 @@
+package git.Algorithm.programmers.lv1;
+
+public class WordToNumReplace {
+    public int solution(String s) {
+        int answer = 0;
+        int len = s.length();
+        String[] digits = {"0","1","2","3","4","5","6","7","8","9"};
+        String[] alphabets = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+
+        for(int i=0; i<10; i++){
+            s = s.replaceAll(alphabets[i],digits[i]);
+        }
+
+        return Integer.parseInt(s);
+    }
+}
