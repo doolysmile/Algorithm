6b4a04ec85bc3fd2d601a98229f27e5203ffc488
KCH <doolysmile1@naver.com>
2022-04-05 23:59:22 +0900

    음양 더하기 - D

---
+++ programmers/lv1/NegativePositivePlus.java
@@ -0,0 +1,14 @@
+package git.Algorithm.programmers.lv1;
+
+public class NegativePositivePlus {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for(int i = 0; i < absolutes.length; i++){
+            if(signs[i] == false){
+                absolutes[i] = absolutes[i]*-1;
+            }
+            answer += absolutes[i];
+        }
+        return answer;
+    }
+}
