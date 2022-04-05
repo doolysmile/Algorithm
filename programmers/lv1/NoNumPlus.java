40a4d30615c03ebc6172f499733604452811b944
KCH <doolysmile1@naver.com>
2022-04-05 23:58:07 +0900

    없는 숫자 더하기 - D

---
+++ programmers/lv1/NoNumPlus.java
@@ -0,0 +1,11 @@
+package git.Algorithm.programmers.lv1;
+
+public class NoNumPlus {
+    public int solution(int[] numbers) {
+        int sum = 45;
+        for (int i : numbers) {
+            sum -= i;
+        }
+        return sum;
+    }
+}
