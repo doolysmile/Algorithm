51a4413bbc53f3aaf7abd408804d022c7f5916b8
KCH <doolysmile1@naver.com>
2022-06-30 23:34:18 +0900

    이중 우선순위 큐 - B(TreeMap 사용, Priority Queue*2, Map 사용은 실패)

--- baekjoon/B7662.java
+++ baekjoon/B7662.java
@@ -4,8 +4,49 @@ import java.io.*;
 import java.util.*;
 
 public class B7662 {
-    public static void main(String[] args) {
-        TreeMap<Integer,Integer>
+    public static void main(String[] args) throws IOException {
+        TreeMap<Integer,Integer> Q;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for(int i = 0; i < T; i++){
+            int k = Integer.parseInt(br.readLine());
+            Q = new TreeMap<>();
+            for(int j = 0; j < k; j++){
+                String[] op = br.readLine().split(" ");
+                int num = Integer.parseInt(op[1]);
+                if(op[0].equals("I")){
+                    Q.put(num, Q.getOrDefault(num, 0) + 1);
+                }
+                else if(op[0].equals("D") &&  Q.size() != 0){
+                    if(num == 1){
+                        int tmp = Q.lastKey();
+                        if(Q.get(tmp) > 1){
+                            Q.put(tmp, Q.getOrDefault(tmp, 0) - 1);
+                        }
+                        else{
+                            Q.pollLastEntry();
+                        }
+                    }
+                    else if(num == -1){
+                        int tmp = Q.firstKey();
+                        if(Q.get(tmp) > 1){
+                            Q.put(tmp, Q.getOrDefault(tmp, 0) - 1);
+                        }
+                        else{
+                            Q.pollFirstEntry();
+                        }
+                    }
+                }
+            }
+            if(Q.size() != 0){
+                sb.append(Q.lastKey() +" " +  Q.firstKey() + "\n");
+            }
+            else {
+                sb.append("EMPTY\n");
+            }
+        }
+        System.out.println(sb.toString());
     }
 //    public static void main(String[] args) throws IOException {
 //        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
