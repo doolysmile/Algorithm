85d749a54efce03368fdaaebc3fd6ccee1ef0079
KCH <doolysmile1@naver.com>
2022-07-20 20:57:43 +0900

    3XN타일링 - A(나머지 계산 하는 부분 잘 보기)

--- programmers/lv3/ThreeMulNTiling.java
+++ programmers/lv3/ThreeMulNTiling.java
@@ -5,36 +5,18 @@ public class ThreeMulNTiling {
         long answer = 11;
         long a = 3;
         long b = 11;
+        if(n%2 ==1){
+            return 0;
+        }
+        if(n == 2){
+            return 3;
+        }
         n = n/2;
         for(int i = 2; i < n; i++){
-            answer = 4*b - a;
+            answer = (4*b% 1000000007 - (a% 1000000007) +1000000007)% 1000000007;
             a = b;
             b = answer;
-
         }
-        return (int)(answer%1000000007);
-    }
-
-    public static int solution(int n) {
-        int answer = 0;
-        int mod = 1000000007;
-        long[] dp = new long[5001];
-
-        dp[0] = 1;
-        dp[2] = 3;
-        for(int i=4; i<=n; i+=2){
-            dp[i] = dp[i-2] * 3;
-            for(int j=i-4; j>=0; j-=2){
-                dp[i] += dp[j] * 2;
-            }
-            dp[i] = dp[i] % mod;
-        }
-
-        answer = (int) dp[n];
-        return answer;
-    }
-    public static void main(String[] args) {
-        ThreeMulNTiling s = new ThreeMulNTiling();
-        System.out.println(s.solution(5000));
+        return (int)answer;
     }
 }
