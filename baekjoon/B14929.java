bb949beb94c806c81657e93e65d4aec1e10447f0
KCH <doolysmile1@naver.com>
2022-10-10 19:33:15 +0900

    귀찮아 (SIB) - D(long형 잘보기, 누적합

---
+++ baekjoon/B14929.java
@@ -0,0 +1,24 @@
+package git.Algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class B14929 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        long N = Integer.parseInt(br.readLine());
+        long mul = 0;
+        long ans = 0;
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            long temp = Integer.parseInt(st.nextToken());
+            ans += temp*mul;
+            mul += temp;
+        }
+        System.out.println(ans);
+    }
+}
