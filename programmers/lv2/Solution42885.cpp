c445a47919d7af7a377daf6714d856ea2190e897
KCH <doolysmile1@naver.com>
2023-08-13 22:29:06 +0900

    구명보트 - A C++(vector 사용법 익히기)

---
+++ programmers/lv2/Solution42885.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+/*
+너무 자바 스럽게 푼거 같음
+vector 메서드 익히기 
+*/
+
+int solution(vector<int> people, int limit) {
+    int answer = people.size();
+    sort(people.begin(), people.end());
+    int j = 0;
+    int maxtmp = answer;
+    for (int i = 0; i < people.size() / 2; i++) {
+        j = maxtmp - 1;
+        while (true) {
+            if (j <= i) {
+                return answer;
+            }
+            if (people[i] + people[j] <= limit) {
+                maxtmp = j;
+                answer--;
+                break;
+            }
+            j--;
+        }
+    }
+    return answer;
+}
